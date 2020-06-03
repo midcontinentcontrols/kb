7d2420b844f3b82768aab9c319b1c01ca1a82c51
Tom Havlik <keres993@gmail.com>
2020-06-03 17:34:10 -0500

    ...

--- pkg/kindest/kaniko.go
+++ pkg/kindest/kaniko.go
@@ -2,9 +2,27 @@ package kindest
 
 import "fmt"
 
-func (b *BuildSpec) buildKaniko(
+func (b *BuildSpec) buildKanikoLocal(
+	manifestPath string,
+	options *BuildOptions,
+) error {
+	return nil
+}
+
+func (b *BuildSpec) buildKanikoRemote(
+	context string,
 	manifestPath string,
 	options *BuildOptions,
 ) error {
 	return fmt.Errorf("unimplemented")
+}
+
+func (b *BuildSpec) buildKaniko(
+	manifestPath string,
+	options *BuildOptions,
+) error {
+	if options.Context != "" {
+		return fmt.Errorf("unimplemented")
+	}
+	return b.buildKanikoLocal(manifestPath, options)
 }
