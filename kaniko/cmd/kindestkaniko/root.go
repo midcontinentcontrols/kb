304e4c2ed1638e35fbb61fe2ea23629b7f943eea
Tom Havlik <keres993@gmail.com>
2020-06-04 14:11:58 -0500

    ...

--- kaniko/cmd/kindestkaniko/root.go
+++ kaniko/cmd/kindestkaniko/root.go
@@ -4,13 +4,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type RootOptions struct {
+	Dest       string
+	Dockerfile string
+	Context    string
+}
+
 // ConfigureCommand a function for adding a command to the application
 func ConfigureCommand(cmd *cobra.Command) {
 	rootCmd.AddCommand(cmd)
 }
 
 var rootCmd = &cobra.Command{
-	Use:   "kindest",
+	Use:   "kindestkaniko",
 	Short: "",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		return nil
