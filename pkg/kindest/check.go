48550d7468fe5798f04c5db56e7e283561831e1d
Tom Havlik <keres993@gmail.com>
2020-06-23 09:50:51 -0500

    ...

--- pkg/kindest/check.go
+++ pkg/kindest/check.go
@@ -1,18 +1,25 @@
 package kindest
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"hash"
 	"sync"
+	"sync/atomic"
+
+	"go.uber.org/zap"
 
 	"github.com/midcontinentcontrols/kindest/pkg/logger"
 )
 
-type BuildStatus string
+type BuildStatus int32
 
 const (
-	BuildStatusPending   BuildStatus = "Pending"
-	BuildStatusFailed    BuildStatus = "Failed"
-	BuildStatusSucceeded BuildStatus = "Succeeded"
+	BuildStatusPending    BuildStatus = 0
+	BuildStatusInProgress BuildStatus = 1
+	BuildStatusFailed     BuildStatus = 2
+	BuildStatusSucceeded  BuildStatus = 3
 )
 
 type resolver struct {
@@ -21,44 +28,213 @@ type resolver struct {
 }
 
 type Module struct {
+	Relative     string
 	ManifestPath string    // absolute path to manifest
 	Dependencies []*Module //
-	Status       BuildStatus
+	status       int32
 	l            sync.Mutex
+	err          error
+	log          logger.Logger
+	subscribersL sync.Mutex
+	subscribers  []chan<- error
 }
 
 func NewModule(
+	relative string,
 	manifestPath string,
 	dependencies []*Module,
 ) *Module {
 	return &Module{
+		Relative:     relative,
 		ManifestPath: manifestPath,
 		Dependencies: dependencies,
-		Status:       BuildStatusPending,
 	}
 }
 
-func (m *Module) Build() (err error) {
-	m.l.Lock()
-	defer func() {
-		if err == nil {
-			m.Status = BuildStatusSucceeded
-		} else {
-			m.Status = BuildStatusFailed
+var ErrModuleNotCached = fmt.Errorf("module is not cached")
+
+func (m *Module) CachedDigest() (string, error) {
+	return "", ErrModuleNotCached
+}
+
+func (m *Module) cacheDigest(digest string) error {
+	return nil
+}
+
+func (m *Module) buildDependencies() error {
+	for _, dependency := range m.Dependencies {
+		if err := dependency.Build(); err != nil {
+			return fmt.Errorf("%s: %v", m.Relative, err)
 		}
-		m.l.Unlock()
-	}()
-	switch m.Status {
-	case BuildStatusPending:
-		break
+	}
+	return nil
+}
+
+func (m *Module) loadBuildContext() (BuildContext, error) {
+	c := make(map[string]interface{})
+	return BuildContext(c), nil
+}
+
+func (m *Module) Status() BuildStatus {
+	return BuildStatus(atomic.LoadInt32(&m.status))
+}
+
+func (m *Module) setStatus(status BuildStatus) error {
+	atomic.StoreInt32(&m.status, int32(status))
+	return nil
+}
+
+func (m *Module) claim() bool {
+	return atomic.CompareAndSwapInt32(
+		&m.status,
+		int32(BuildStatusPending),
+		int32(BuildStatusInProgress),
+	)
+}
+
+func (m *Module) subscribe(done chan<- error) {
+	m.subscribersL.Lock()
+	defer m.subscribersL.Unlock()
+	switch m.Status() {
+	case BuildStatusInProgress:
+		m.subscribers = append(m.subscribers, done)
 	case BuildStatusFailed:
-		return fmt.Errorf("build failed")
+		done <- m.err
 	case BuildStatusSucceeded:
-		return nil
+		done <- nil
 	default:
 		panic("unreachable")
 	}
+}
+
+func (m *Module) broadcast(err error) {
+	m.subscribersL.Lock()
+	defer m.subscribersL.Unlock()
+	m.err = err
+	for _, subscriber := range m.subscribers {
+		subscriber <- err
+	}
+}
+
+func (m *Module) WaitForCompletion() error {
+	done := make(chan error, 1)
+	m.subscribe(done)
+	return <-done
+}
+
+func (m *Module) Build() (err error) {
+	if !m.claim() {
+		switch m.Status() {
+		case BuildStatusInProgress:
+			return m.WaitForCompletion()
+		case BuildStatusFailed:
+			if m.err == nil {
+				panic("unreachable")
+			}
+			return m.err
+		case BuildStatusSucceeded:
+			return nil
+		default:
+			panic("unreachable")
+		}
+	}
+	defer func() {
+		if err == nil {
+			err = m.setStatus(BuildStatusSucceeded)
+		} else {
+			m.setStatus(BuildStatusFailed)
+		}
+		m.broadcast(err)
+	}()
+	if err := m.buildDependencies(); err != nil {
+		return err
+	}
+	buildContext, err := m.loadBuildContext()
+	if err != nil {
+		return err
+	}
+	digest, err := buildContext.Digest()
+	if err != nil {
+		return err
+	}
+	cachedDigest, err := m.CachedDigest()
+	if err != nil && err != ErrModuleNotCached {
+		return err
+	}
+	if digest == cachedDigest {
+		m.log.Info("No files changed", zap.String("digest", cachedDigest))
+		return nil
+	}
+	// TODO: actually do the building
+	if err := m.cacheDigest(digest); err != nil {
+		return err
+	}
 	return nil
+}
+
+type BuildContext map[string]interface{}
+
+func hashContext(context map[string]interface{}, h hash.Hash, prefix string) error {
+	for k, v := range context {
+		k = prefix + k
+		if file, ok := v.(*File); ok {
+			if _, err := h.Write([]byte(k)); err != nil {
+				return err
+			}
+			if _, err := h.Write([]byte{
+				byte((file.Permissions >> 24) & 0xFF),
+				byte((file.Permissions >> 16) & 0xFF),
+				byte((file.Permissions >> 8) & 0xFF),
+				byte((file.Permissions >> 0) & 0xFF),
+			}); err != nil {
+				return err
+			}
+			if _, err := h.Write(file.Content); err != nil {
+				return err
+			}
+		} else if dir, ok := v.(*Directory); ok {
+			k = k + "?" // impossible to use in most filesystems
+			if _, err := h.Write([]byte(k)); err != nil {
+				return err
+			}
+			if _, err := h.Write([]byte{
+				byte((dir.Permissions >> 24) & 0xFF),
+				byte((dir.Permissions >> 16) & 0xFF),
+				byte((dir.Permissions >> 8) & 0xFF),
+				byte((dir.Permissions >> 0) & 0xFF),
+			}); err != nil {
+				return err
+			}
+			if err := hashContext(dir.Contents, h, k); err != nil {
+				return err
+			}
+		} else {
+			panic("unreachable")
+		}
+	}
+	return nil
+}
+
+func (c BuildContext) Digest() (string, error) {
+	h := md5.New()
+	if err := hashContext(map[string]interface{}(c), h, ""); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
+
+func (c BuildContext) Archive() ([]byte, error) {
+	return nil, nil
+}
+
+type File struct {
+	Permissions int
+	Content     []byte
+}
+
+type Directory struct {
+	Permissions int
+	Contents    map[string]interface{}
 }
 
 type CheckOptions struct {
