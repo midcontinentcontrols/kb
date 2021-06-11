b6dc26f5b34e4efe334ae059e1a62c4dd4dbe346
Tom Havlik <thavlik@protonmail.com>
2021-06-11 08:32:58 -0500

    ...

--- pkg/kindest/test_test.go
+++ pkg/kindest/test_test.go
@@ -15,42 +15,48 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-/*
 func TestTestDockerEnv(t *testing.T) {
-	files := func(name string) map[string]interface{} {
-		return map[string]interface{}{
-			"Dockerfile": `FROM alpine:latest
-CMD ["sh", "-c", "set -eu; echo $MESSAGE"]`,
-			"kindest.yaml": fmt.Sprintf(`build:
+	name := "test-" + uuid.New().String()[:8]
+	rootPath := filepath.Join("tmp", name)
+	require.NoError(t, os.MkdirAll(rootPath, 0766))
+	defer os.RemoveAll(rootPath)
+	dockerfile := `FROM alpine:latest
+CMD ["sh", "-c", "set -euo pipefail; echo $MYVARIABLE"]`
+	require.NoError(t, ioutil.WriteFile(
+		filepath.Join(rootPath, "Dockerfile"),
+		[]byte(dockerfile),
+		0644,
+	))
+	specPath := filepath.Join(rootPath, "kindest.yaml")
+	spec := fmt.Sprintf(`build:
   name: test/%s
 test:
   - name: basic
-env:
-  variables:
-    - name: MESSAGE
-      value: "It works!"
-  docker: {}
-  build:
-    name: midcontinentcontrols/kindest-basic-test
-    dockerfile: Dockerfile
-`, name),
-		}
-	}
-	runTest(
-		t,
-		files,
-		func(t *testing.T, rootPath string) {
-			require.NoError(t, Test(
-				&TestOptions{
-					File:       filepath.Join(rootPath, "kindest.yaml"),
-					NoRegistry: true,
-				},
-				test.NewTestLogger(),
-			))
-		},
-	)
+    env:
+      variables:
+        - name: MYVARIABLE
+          value: foobarbaz
+      docker: {}
+    build:
+      name: test/%s-test
+      dockerfile: Dockerfile
+`, name, name)
+	require.NoError(t, ioutil.WriteFile(
+		specPath,
+		[]byte(spec),
+		0644,
+	))
+	log := logger.NewMockLogger(logger.NewFakeLogger())
+	p := NewProcess(runtime.NumCPU(), log)
+	module, err := p.GetModule(specPath)
+	require.NoError(t, err)
+	require.NoError(t, module.Build(&BuildOptions{NoPush: true}))
+	test, err := p.GetModuleFromBuildSpec(specPath, &module.Spec.Test[0].Build)
+	require.NoError(t, err)
+	require.NoError(t, test.Build(&BuildOptions{NoPush: true}))
+	err = module.Spec.RunTests(&TestOptions{}, log)
+	require.NoError(t, err)
 }
-*/
 
 func TestTestErrNoEnv(t *testing.T) {
 	name := "test-" + uuid.New().String()[:8]
