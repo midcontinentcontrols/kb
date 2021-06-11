26f36b466924fcd34b9c5202f14b7cb3ae2b347d
Tom Havlik <thavlik@protonmail.com>
2021-06-11 08:37:35 -0500

    ...

---
+++ pkg/kindest/test_docker_test.go
@@ -0,0 +1,100 @@
+package kindest
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"testing"
+
+	"github.com/midcontinentcontrols/kindest/pkg/logger"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTestDockerEnv(t *testing.T) {
+	name := "test-" + uuid.New().String()[:8]
+	rootPath := filepath.Join("tmp", name)
+	require.NoError(t, os.MkdirAll(rootPath, 0766))
+	defer os.RemoveAll(rootPath)
+	dockerfile := `FROM alpine:latest
+CMD ["sh", "-c", "set -euo pipefail; echo $MYVARIABLE"]`
+	require.NoError(t, ioutil.WriteFile(
+		filepath.Join(rootPath, "Dockerfile"),
+		[]byte(dockerfile),
+		0644,
+	))
+	specPath := filepath.Join(rootPath, "kindest.yaml")
+	spec := fmt.Sprintf(`build:
+  name: test/%s
+test:
+  - name: basic
+    env:
+      variables:
+        - name: MYVARIABLE
+          value: foobarbaz
+      docker: {}
+    build:
+      name: test/%s-test
+      dockerfile: Dockerfile
+`, name, name)
+	require.NoError(t, ioutil.WriteFile(
+		specPath,
+		[]byte(spec),
+		0644,
+	))
+	log := logger.NewMockLogger(logger.NewFakeLogger())
+	p := NewProcess(runtime.NumCPU(), log)
+	module, err := p.GetModule(specPath)
+	require.NoError(t, err)
+	require.NoError(t, module.Build(&BuildOptions{NoPush: true}))
+	test, err := p.GetModuleFromBuildSpec(specPath, &module.Spec.Test[0].Build)
+	require.NoError(t, err)
+	require.NoError(t, test.Build(&BuildOptions{NoPush: true}))
+	err = module.Spec.RunTests(&TestOptions{}, log)
+	require.NoError(t, err)
+}
+
+func TestTestErrorDocker(t *testing.T) {
+	name := "test-" + uuid.New().String()[:8]
+	rootPath := filepath.Join("tmp", name)
+	require.NoError(t, os.MkdirAll(rootPath, 0766))
+	defer os.RemoveAll(rootPath)
+	dockerfile := `FROM alpine:latest
+CMD ["sh", "-c", "exit 1"]`
+	require.NoError(t, ioutil.WriteFile(
+		filepath.Join(rootPath, "Dockerfile"),
+		[]byte(dockerfile),
+		0644,
+	))
+	specPath := filepath.Join(rootPath, "kindest.yaml")
+	spec := fmt.Sprintf(`build:
+  name: test/%s
+test:
+  - name: basic
+    env:
+      docker: {}
+    build:
+      name: test/%s-test
+      dockerfile: Dockerfile
+`, name, name)
+	require.NoError(t, ioutil.WriteFile(
+		specPath,
+		[]byte(spec),
+		0644,
+	))
+	log := logger.NewMockLogger(logger.NewFakeLogger())
+	p := NewProcess(runtime.NumCPU(), log)
+	module, err := p.GetModule(specPath)
+	require.NoError(t, err)
+	require.NoError(t, module.Build(&BuildOptions{NoPush: true}))
+	test, err := p.GetModuleFromBuildSpec(specPath, &module.Spec.Test[0].Build)
+	require.NoError(t, err)
+	require.NoError(t, test.Build(&BuildOptions{NoPush: true}))
+	err = module.Spec.RunTests(&TestOptions{}, log)
+	require.Error(t, err)
+	require.Truef(t, strings.Contains(err.Error(), "exit code 1"), "got error '%s'", err.Error())
+}
